aa5bc1a6fbb59bf1a0c93632e574fb133fef4000
Zhuravlov Serhii <gadget.krm@gmail.com>
2020-06-17 17:41:27 +0300

    v1.03_add_login

--- login.php
+++ login.php
@@ -1,67 +1,48 @@
 <?php
-include $_SERVER['DOCUMENT_ROOT'] .'/configs/db.php'; 
 
-    if(isset($_GET['u_code'])) {
-    $sql = "SELECT * FROM users WHERE confirm_mail='" . $_GET['u_code'] . "'";
-    $result = $connect->query($sql);
-    if($result->num_rows > 0) {
-        $user = mysqli_fetch_assoc($result);
-        $sql = "UPDATE users SET verified = '1' , confirm_mail = '' WHERE id_user=" . $user['id_user'];
+INCLUDE $_SERVER['DOCUMENT_ROOT'] .'/configs/db.php';
 
-        if($connect->query($sql)){ 
-            header('Location: http://real-madrid_fan-shop.local/index.php');
-        }
-    }
-}
-
-if(isset($_POST) and $_SERVER["REQUEST_METHOD"]=="POST") {
-    //registration
+if(
+    isset($_POST["email"]) && isset($_POST["password"])
+    && $_POST["email"] != "" && $_POST["password"] != ""
+) {
     $password = md5($_POST['password']);
-
-    $u_code = generateRandomString(20);
-
-    $sql = " INSERT INTO users (login, email, confirm_mail, password) 
-    VALUES ('" . $_POST['username'] . "', '" . $_POST['email'] . "', 
-            '$u_code', '" . $password . "')"; 
-    $result = $connect->query($sql);
-    if($result){
-        echo "User registred";
-        $link = "<a href='http://real-madrid_fan-shop.local/login.php?u_code=$u_code'>Confirm</a>";
-        mail($_POST['email'], 'Register', $link);
-        header('Location: http://real-madrid_fan-shop.local/index.php');
+    $sql = "SELECT * FROM users WHERE email LIKE '" . $_POST["email"] . "' AND password LIKE '" . $password .  "'";
+    $result = mysqli_query($connect, $sql);
+    $number_ofusers = mysqli_num_rows($result);
+    if ($number_ofusers == 1) {
+        $user = mysqli_fetch_assoc($result);
+        setcookie("user_id", $user["id_user"], time() + 600);
+        header("Location: /"); 
+    } else {
+        echo "<h2>Логин или пароль введены не верно</h2>";
     }
 }
-
-function generateRandomString($Lenght = 10) {
-    $characters = '0123456789abcdefghijklmnopqrstuvwzyxABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    $characterLenght = strlen($characters);
-    $randomString = '';
-    for ($i = 0; $i < $Lenght; $i++) {
-        $randomString .= $characters[rand(0, $characterLenght - 1)];
-    }
-    return $randomString;
-}
-
 ?>
 <!DOCTYPE html>
 <html>
 <head>
+    <title>Autorisation</title>
     <link rel="stylesheet" href="assets/css/bootstrap.min.css" href="assets/style.css"/>
-    <title>Registrarion</title>
 </head>
 <body>
-<form method="POST">
-    <p>Username<br/>
-        <input type="text" name="username">
-    </p>
-    <p>E-mail<br/>
-        <input type="text" name="email">
-    </p>
-    <p>Password<br/>
-        <input type="password" name="password">
-    </p>
-    <button type="submit">Register</button> 
-</form>
-
+    <div id="content">
+        <h2>Autorisation</h2>
+        <form class="form-inline my-2 my-lg-0 col-3" action="login.php" method="POST">
+            <p>
+                Please input your email: <br/>
+                <input class="form-control mr-sm-2" type="text" name="email">
+            </p>
+            <p>
+                Please input your password: <br/>
+                <input class="form-control mr-sm-2" type="text" name="password">
+            </p>
+            <P><br/>
+                <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">LOG IN</button>
+            </P>
+        </form> 
+        
+        <a href="register.php">    Registration</a>
+    </div>
 </body>
 </html>
---
+++ register.php
@@ -0,0 +1,70 @@
+<?php
+include $_SERVER['DOCUMENT_ROOT'] .'/configs/db.php'; 
+
+    if(isset($_GET['u_code'])) {
+    $sql = "SELECT * FROM users WHERE confirm_mail='" . $_GET['u_code'] . "'";
+    $result = $connect->query($sql);
+    if($result->num_rows > 0) {
+        $user = mysqli_fetch_assoc($result);
+        $sql = "UPDATE users SET verified = '1' , confirm_mail = '' WHERE id_user=" . $user['id_user'];
+
+        if($connect->query($sql)){ 
+            header('Location: http://real-madrid_fan-shop.local/index.php');
+        }
+    }
+}
+
+if(isset($_POST) and $_SERVER["REQUEST_METHOD"]=="POST") {
+    //registration
+    $password = md5($_POST['password']);
+
+    $u_code = generateRandomString(20);
+
+    $sql = " INSERT INTO users (login, email, confirm_mail, password) 
+    VALUES ('" . $_POST['username'] . "', '" . $_POST['email'] . "', 
+            '$u_code', '" . $password . "')"; 
+    $result = $connect->query($sql);
+    if($result){
+        echo "User registred";
+        $link = "<a href='http://real-madrid_fan-shop.local/register.php?u_code=$u_code'>Confirm</a>";
+        mail($_POST['email'], 'Register', $link);
+        header('Location: http://real-madrid_fan-shop.local/index.php');
+    }
+}
+
+function generateRandomString($Lenght = 10) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwzyxABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $characterLenght = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $Lenght; $i++) {
+        $randomString .= $characters[rand(0, $characterLenght - 1)];
+    }
+    return $randomString;
+}
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css" href="assets/style.css"/>
+    <title>Registrarion</title>
+</head>
+<body>
+<div id="content">
+    <h2>Registrarion</h2>
+    <form class="form-inline my-2 my-lg-0 col-3" method="POST">
+        <p>Login<br/>
+            <input class="form-control mr-sm-2" type="text" name="username">
+        </p>
+        <p>E-mail<br/>
+            <input class="form-control mr-sm-2" type="text" name="email">
+        </p>
+        <p>Password<br/>
+            <input class="form-control mr-sm-2" type="password" name="password">
+        </p>
+        <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Register</button> 
+    </form>
+</div>
+
+</body>
+</html>
