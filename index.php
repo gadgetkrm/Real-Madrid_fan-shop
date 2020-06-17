0142386902914a5820834bbbb211ef430723528e
Zhuravlov Serhii <gadget.krm@gmail.com>
2020-06-17 12:30:07 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css"/>
+	<title>Real Madrid fan-shop</title>
+</head>
+<body>
+
+	<!-- Header ================================================================================= -->
+	<nav class="navbar navbar-expand-lg navbar-light-blue mt-3">
+
+		<div class="col-6">
+			<a class="navbar-brand col-8" href="/" style="border: 1px solid; border-color: blue;">
+				<img src="pics/logo.png" width="120" href="/"><vr/>
+				Real Madrid fan-shop
+			</a>
+		</div>
+
+		<form class="form-inline my-2 my-lg-0 col-3">
+			<input class="form-control mr-sm-2" type="search" placeholder="Search">
+			<button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>
+		</form>	
+
+		<div class="collapse navbar-collapse col-1" id="navbarTogglerDemo02">
+			<ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+				<li class="nav-item">
+					<a class="nav-link" href="#">Basket<span class="ml-2">0</span></a>
+				</li>
+			</ul>
+		</div>
+
+		<div class="collapse navbar-collapse col-2 mr-2" id="navbarTogglerDemo02">
+			<ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+				<li class="nav-item active">
+					<a class="nav-link" href="#">Log in/Register</a>
+				</li>
+			</ul>
+		</div>		
+	</nav>
+	<hr/class="mb-0">
+
+	<!--Categories ======================================================================== -->
+
+	<div class="row col-11">
+		<div class="col-4">
+		</div>
+		<div class="col-7">
+			<div id="list-example" class="row" style="font-size: 18px;">
+				<a class="nav-link btn-outline-info" href="squad.php">Squad</a>
+				<a class="nav-link btn-outline-info" href="#list-item-2">Kits</a>
+				<a class="nav-link btn-outline-info" href="#list-item-3">Training</a>
+				<a class="nav-link btn-outline-info" href="#list-item-4">Apparel</a>
+				<a class="nav-link btn-outline-info" href="#list-item-3">Souvenirs</a>
+				<a class="nav-link btn-outline-info" href="#list-item-4">Tickets</a>
+			</div>
+		</div>
+	</div>
+	<hr/ class="mt-0">
+
+	<!-- body============================================================================== -->
+
+	<div class="row col-12 mt-4">
+		<div class="col-2">
+		</div>
+		<div class="col-8">
+			<div class="embed-responsive embed-responsive-16by9">
+				<iframe class="embed-responsive-item" src="pics/goal-video2.mp4"></iframe>
+			</div>
+		</div>
+	</div>
+
+
+	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+	<script src="assets/js/bootstrap.min.js"></script>
+	<script src="assets/js/main.js"></script>
+</body>
+</html>
+
+
+
---
+++ squad4.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css"/>
+	<link rel="stylesheet" href="assets/css/stl.css"/>
+	<title>Real Madrid fan-shop</title>
+</head>
+<body>
+
+<!-- Header ================================================================================= -->
+<nav class="navbar navbar-expand-lg navbar-light-blue mt-3">
+
+	<div class="col-6">
+		<a class="navbar-brand col-8" href="/" style="border: 1px solid; border-color: blue;">
+			<img src="pics/logo.png" width="120" href="/"><vr/>
+			Real Madrid fan-shop
+		</a>
+	</div>
+
+	<form class="form-inline my-2 my-lg-0 col-3">
+		<input class="form-control mr-sm-2" type="search" placeholder="Search">
+		<button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>
+	</form>	
+
+	<div class="collapse navbar-collapse col-1" id="navbarTogglerDemo02">
+		<ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+			<li class="nav-item">
+				<a class="nav-link" href="#">Basket<span class="ml-2">0</span></a>
+			</li>
+		</ul>
+	</div>
+
+	<div class="collapse navbar-collapse col-2 mr-2" id="navbarTogglerDemo02">
+		<ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+			<li class="nav-item active">
+				<a class="nav-link" href="#">Log in/Register</a>
+			</li>
+		</ul>
+	</div>		
+</nav>
+<hr/class="mb-0">
+
+<!-- SQUAD =============================================================================== -->
+<div class="container mt-4">
+    <div class="row">
+        <div class="col-md-12">
+            <div id="grid" class="row">
+                <div class="mix col-sm-3 page1 page4 margin30">
+                    <div class="item-img-wrap ">
+                        <img src="pics/vinicius.png" alt="workimg">
+                        <div class="item-img-overlay">
+                            <a href="#" class="show-image">
+                                <span></span>
+                            </a>
+                        </div>
+                    </div> 
+
+                </div>
+                <div class="mix col-sm-3 page2 page3 margin30">
+                    <div class="item-img-wrap ">
+                        <img src="pics/rodrygo.png" class="img-responsive" alt="workimg">
+                        <div class="item-img-overlay">
+                            <a href="#" class="show-image">
+                                <span></span>
+                            </a>
+                        </div>
+                    </div> 
+
+                </div>                                                        
+            </div><!--grid-->
+        </div>
+    </div>
+    <div class="row gallery-bottom col-12 ml-2">
+        <div class="col-1">
+        </div>
+        <div class="col-2 ml-5">
+            <div class="col-sm-6">
+                <ul class="pagination">
+                    <li>
+                        <a href="squad3.php" aria-label="Previous">
+                            <span aria-hidden="true">«</span>
+                        </a>
+                    </li>
+                    <li><a href="squad.php">1</a></li>
+                    <li><a href="squad2.php">2</a></li>
+                    <li><a href="squad3.php">3</a></li>
+                    <li class="active"><a href="squad4.php">4</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
